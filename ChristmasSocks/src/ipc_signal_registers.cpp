9206d68e8f34ba8dbbc63e9046881c63078edd4a
David Weber <dsw7@sfu.ca>
2021-05-02 06:42:06 +0100

    Add source

---
+++ ChristmasSocks/src/ipc_signal_registers.cpp
@@ -0,0 +1,19 @@
+#include "ipc_signal_registers.h"
+
+void sigint_handler(int signal) {
+    Logger::info("Interrupt signal SIGINT (" + std::to_string(signal) + ") received!");
+    information_footer();
+    exit(signal);
+}
+
+void sigterm_handler(int signal) {
+    Logger::info("Termination signal SIGTERM (" + std::to_string(signal) + ") received!");
+    information_footer();
+    exit(signal);
+}
+
+void register_ipc_signals() {
+    Logger::info("Registering IPC signals to process");
+    std::signal(SIGINT, sigint_handler);
+    std::signal(SIGTERM, sigterm_handler);
+};
