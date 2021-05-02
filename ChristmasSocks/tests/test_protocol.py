8edca411d7558d443fcac145b97d31027219f0b3
David Weber <dsw7@sfu.ca>
2021-05-02 07:07:47 +0100

    Add constructor destructor to test

--- ChristmasSocks/tests/test_protocol.py
+++ ChristmasSocks/tests/test_protocol.py
@@ -6,33 +6,24 @@ from utils import (
 
 
 class TestProtocolRandomStrings(TestCase):
+    def setUp(self) -> None:
+        self.socket = Connection()
+        self.socket.connect()
+
+    def tearDown(self) -> None:
+        self.socket.disconnect()
 
     def test_echo_5_byte_string(self):
-        socket = Connection()
-        socket.connect()
-
         for string in generate_random_string(num_strings=5, len_strings=5):
             with self.subTest():
-                self.assertEqual(string, socket.send(string))
-
-        socket.disconnect()
+                self.assertEqual(string, self.socket.send(string))
 
     def test_echo_10_byte_string(self):
-        socket = Connection()
-        socket.connect()
-
         for string in generate_random_string(num_strings=5, len_strings=10):
             with self.subTest():
-                self.assertEqual(string, socket.send(string))
-
-        socket.disconnect()
+                self.assertEqual(string, self.socket.send(string))
 
     def test_echo_15_byte_string(self):
-        socket = Connection()
-        socket.connect()
-
         for string in generate_random_string(num_strings=5, len_strings=15):
             with self.subTest():
-                self.assertEqual(string, socket.send(string))
-
-        socket.disconnect()
+                self.assertEqual(string, self.socket.send(string))
