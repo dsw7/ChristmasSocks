940338065e217d1f005dd6164589910d15115866
David Weber <dsw7@sfu.ca>
2021-05-03 03:19:33 +0100

    Typing

--- ChristmasSocks/tests/test_protocol.py
+++ ChristmasSocks/tests/test_protocol.py
@@ -13,17 +13,17 @@ class TestProtocolRandomStrings(TestCase):
     def tearDown(self) -> None:
         self.socket.disconnect()
 
-    def test_echo_5_byte_string(self):
+    def test_echo_5_byte_string(self) -> None:
         for string in generate_random_string(num_strings=5, len_strings=5):
             with self.subTest():
                 self.assertEqual(string, self.socket.send(string))
 
-    def test_echo_10_byte_string(self):
+    def test_echo_10_byte_string(self) -> None:
         for string in generate_random_string(num_strings=5, len_strings=10):
             with self.subTest():
                 self.assertEqual(string, self.socket.send(string))
 
-    def test_echo_15_byte_string(self):
+    def test_echo_15_byte_string(self) -> None:
         for string in generate_random_string(num_strings=5, len_strings=15):
             with self.subTest():
                 self.assertEqual(string, self.socket.send(string))
