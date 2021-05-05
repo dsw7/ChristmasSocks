2d568e51a2779062b48daa03b1f74d8736da13cc
David Weber <dsw7@sfu.ca>
2021-05-05 05:10:37 +0100

    Add client handler to namespace

--- ChristmasSocks/src/clients.cpp
+++ ChristmasSocks/src/clients.cpp
@@ -1,66 +1,70 @@
 #include "clients.h"
 
-bool accept_incoming_connection(int &socket_fd_server, struct sockaddr_in &address, int &socket_fd_client) {
-    int addrlen = sizeof(address);
+namespace ClientHandler {
 
-    // https://linux.die.net/man/3/accept
-    socket_fd_client = accept(
-        socket_fd_server, (struct sockaddr *)&address, (socklen_t*)&addrlen
-    );
+    /* https://linux.die.net/man/3/accept */
+    bool accept_incoming_connection(int &socket_fd_server, struct sockaddr_in &address, int &socket_fd_client) {
+        int addrlen = sizeof(address);
 
-    if (socket_fd_client == -1) {
-        Logger::error(strerror(errno));
-        return false;
+        socket_fd_client = accept(
+            socket_fd_server, (struct sockaddr *)&address, (socklen_t*)&addrlen
+        );
+
+        if (socket_fd_client == -1) {
+            Logger::error(strerror(errno));
+            return false;
+        }
+
+        char *incoming_ipv4_address = inet_ntoa(address.sin_addr);
+        Logger::info("The kernel has allocated a new client socket file descriptor: " + std::to_string(socket_fd_client));
+        Logger::info("Accepted connection from IPv4 address " + std::string(incoming_ipv4_address));
+
+        return true;
     }
 
-    char *incoming_ipv4_address = inet_ntoa(address.sin_addr);
-    Logger::info("The kernel has allocated a new client socket file descriptor: " + std::to_string(socket_fd_client));
-    Logger::info("Accepted connection from IPv4 address " + std::string(incoming_ipv4_address));
+    /* https://linux.die.net/man/3/close */
+    bool close_client_socket_file_descriptor(int &socket_fd_client) {
+        Logger::info("Closing client socket file descriptor: " + std::to_string(socket_fd_client));
 
-    return true;
-}
-
-bool close_client_socket_file_descriptor(int &socket_fd_client) {
-    Logger::info("Closing client socket file descriptor: " + std::to_string(socket_fd_client));
-
-    // https://linux.die.net/man/3/close
-    if (close(socket_fd_client) == -1) {
-        Logger::error(strerror(errno));
-        return false;
-    }
-    return true;
-}
-
-bool read_data(std::string &message, int &socket_fd_client) {
-    char buffer[TCP_BUFFER_SIZE] = {0};
-
-    // https://linux.die.net/man/3/read
-    int rv = read(socket_fd_client, buffer, TCP_BUFFER_SIZE);
-    message = std::string(buffer);
-
-    if (rv < 0) {
-        Logger::error(strerror(errno));
-        return false;
-    } else if (rv == 0) {
-        Logger::info("Socket read() received EOF - client hang up detected");
-        close_client_socket_file_descriptor(socket_fd_client);
-        return false;
-    } else {
-        Logger::info("Read in message '" + message + "' from descriptor " + std::to_string(socket_fd_client));
+        if (close(socket_fd_client) == -1) {
+            Logger::error(strerror(errno));
+            return false;
+        }
+        return true;
     }
 
-    return true;
-}
+    /* https://linux.die.net/man/3/read */
+    bool read_data(std::string &message, int &socket_fd_client) {
+        char buffer[TCP_BUFFER_SIZE] = {0};
 
-bool write_data(std::string &message, int &socket_fd_client) {
-    // https://linux.die.net/man/3/send
-    int rv = send(socket_fd_client, message.c_str(), message.size(), 0);
+        int rv = read(socket_fd_client, buffer, TCP_BUFFER_SIZE);
+        message = std::string(buffer);
 
-    if (rv == -1) {
-        Logger::error(strerror(errno));
-        return false;
+        if (rv < 0) {
+            Logger::error(strerror(errno));
+            return false;
+        } else if (rv == 0) {
+            Logger::info("Socket read() received EOF - client hang up detected");
+            close_client_socket_file_descriptor(socket_fd_client);
+            return false;
+        } else {
+            Logger::info("Read in message '" + message + "' from descriptor " + std::to_string(socket_fd_client));
+        }
+
+        return true;
     }
 
-    Logger::info("Wrote out message '" + message + "' to descriptor " + std::to_string(socket_fd_client));
-    return true;
+    /* https://linux.die.net/man/3/send */
+    bool write_data(std::string &message, int &socket_fd_client) {
+        int rv = send(socket_fd_client, message.c_str(), message.size(), 0);
+
+        if (rv == -1) {
+            Logger::error(strerror(errno));
+            return false;
+        }
+
+        Logger::info("Wrote out message '" + message + "' to descriptor " + std::to_string(socket_fd_client));
+        return true;
+    }
+
 }
