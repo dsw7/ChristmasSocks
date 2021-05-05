9b78c94a97c1239cb0d059a6dbe76bfb85dc2c69
David Weber <dsw7@sfu.ca>
2021-05-05 06:13:27 +0100

    Add RT to prefix

--- ChristmasSocks/src/logger.cpp
+++ ChristmasSocks/src/logger.cpp
@@ -4,23 +4,23 @@ namespace RootLogger {
 
     void debug(std::string message) {
 #if DEBUG_BUILD == 1
-        std::string line = get_current_datetime_string() + " D " + message;
+        std::string line = get_current_datetime_string() + " D RT " + message;
         std::cout << line << std::endl;
 #endif
     }
 
     void info(std::string message) {
-        std::string line = get_current_datetime_string() + " I " + message;
+        std::string line = get_current_datetime_string() + " I RT " + message;
         std::cout << line << std::endl;
     }
 
     void warning(std::string message) {
-        std::string line = get_current_datetime_string() + " W " + message;
+        std::string line = get_current_datetime_string() + " W RT " + message;
         std::cout << line << std::endl;
     }
 
     void error(std::string message) {
-        std::string line = get_current_datetime_string() + " E " + message;
+        std::string line = get_current_datetime_string() + " E RT " + message;
         std::cout << line << std::endl;
     }
 
