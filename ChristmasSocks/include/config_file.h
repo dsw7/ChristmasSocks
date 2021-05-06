6c673790df955a4e53c277baaa0129d8946f3d51
David Weber <dsw7@sfu.ca>
2021-05-06 08:57:30 +0100

    Add config file struct

--- ChristmasSocks/include/config_file.h
+++ ChristmasSocks/include/config_file.h
@@ -5,6 +5,13 @@
 #include "constants.h"
 #include "logger.h"
 
+struct configs_t {
+    bool enable_debug_logging;
+    unsigned int tcp_port;
+    unsigned int max_num_connections_queue;
+    unsigned int tcp_buffer_size;
+};
+
 void read_config_file();
 
 #endif
