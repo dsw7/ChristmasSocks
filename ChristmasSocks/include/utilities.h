972e0b4fbc43a85fa32b9e0f3111c0d3d3d500e5
David Weber <dsw7@sfu.ca>
2021-05-07 08:32:40 +0100

    Add config getter

--- ChristmasSocks/include/utilities.h
+++ ChristmasSocks/include/utilities.h
@@ -4,9 +4,13 @@
 #include <string>
 #include <time.h>
 #include <sys/stat.h>
+#include <iostream>
+#include <sstream>
+#include <map>
 
 std::string get_current_datetime_string();
 bool file_exists(std::string &pathname);
 bool read_file(std::string &filepath, std::string &file_contents);
+void parse_configs(std::string &file_contents, std::map<std::string, std::string> &configs);
 
 #endif
