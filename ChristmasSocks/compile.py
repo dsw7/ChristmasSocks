74249224771676ee96b7057a093b45e57eca883d
David Weber <dsw7@sfu.ca>
2021-05-09 10:23:26 +0100

    py compiler

---
+++ ChristmasSocks/compile.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import sys
+from os import get_terminal_size, path
+from subprocess import call
+
+EXIT_SUCCESS = 0
+EXIT_FAILURE = 1
+
+
+class CompileSocks:
+    def __init__(self) -> None:
+        self.hbar = '=' * get_terminal_size().columns
+        self.path_this = path.dirname(__file__)
+
+    def generate_makefiles(self) -> int:
+        command = 'cmake -S {} -B {}/bin'.format(self.path_this)
+        return call(command.split())
+
+    def compile_binary_from_makefiles(self) -> int:
+        command = 'make --jobs=12 -C {}/bin'.format(self.path_this)
+        return call(command.split())
+
+    def execute_main(self) -> None:
+        print(self.hbar)
+        print('Compiling project ChristmasSocks')
+
+        if self.generate_makefiles() != EXIT_SUCCESS:
+            return EXIT_FAILURE
+
+        if self.compile_binary_from_makefiles() != EXIT_SUCCESS:
+            return EXIT_FAILURE
+
+        print(self.hbar)
+        return EXIT_SUCCESS
+
+
+def main():
+    sys.exit(CompileSocks().execute_main())
+
+if __name__ == '__main__':
+    main()
