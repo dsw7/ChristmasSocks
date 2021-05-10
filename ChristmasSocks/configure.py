5b9de23db90cd33fa8235090e3a4416f1e74762f
David Weber <dsw7@sfu.ca>
2021-05-10 04:00:08 +0100

    ABC

--- ChristmasSocks/configure.py
+++ ChristmasSocks/configure.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-
 import sys
 from os import get_terminal_size, path
+from abc import ABC, abstractmethod
 from subprocess import call
 from time import time
 from click import (
@@ -12,32 +12,41 @@ from click import (
 
 EXIT_SUCCESS = 0
 EXIT_FAILURE = 1
-TERMINAL_WIDTH = get_terminal_size().columns
-
-def render_separator() -> None:
-    secho('=' * TERMINAL_WIDTH, fg='magenta')
 
 
-class CompileSocks:
-    def __init__(self, num_jobs: int) -> None:
+class ConfigBase(ABC):
+    def __init__(self) -> None:
         self.path_this = path.dirname(__file__)
         self.start_time = time()
-        self.num_jobs = num_jobs
+        self.separator = '=' * get_terminal_size().columns
 
+    def render_separator(self) -> None:
+        secho(self.separator, fg='cyan')
+
+    @staticmethod
+    def render_message(msg: str) -> None:
+        secho('** {}'.format(msg), fg='cyan')
+
+    @abstractmethod
+    def execute_main(self) -> int:
+        pass
+
+
+class CompileSocks(ConfigBase):
     def generate_makefiles(self) -> int:
-        render_separator()
+        self.render_separator()
         command = 'cmake -S {} -B {}/bin'.format(self.path_this, self.path_this)
         return call(command.split())
 
     def compile_binary_from_makefiles(self) -> int:
-        render_separator()
-        command = 'make --jobs={} -C {}/bin'.format(self.num_jobs, self.path_this)
+        self.render_separator()
+        command = 'make --jobs=12 -C {}/bin'.format(self.path_this)
         return call(command.split())
 
     def execute_main(self) -> None:
-        render_separator()
-        secho('-- Compiling project: ChristmasSocks')
-        secho('-- Project maintainer: David Weber')
+        self.render_separator()
+        self.render_message('Compiling project: ChristmasSocks')
+        self.render_message('Project maintainer: David Weber')
 
         if self.generate_makefiles() != EXIT_SUCCESS:
             return EXIT_FAILURE
@@ -45,9 +54,9 @@ class CompileSocks:
         if self.compile_binary_from_makefiles() != EXIT_SUCCESS:
             return EXIT_FAILURE
 
-        render_separator()
-        secho('-- Success!')
-        secho('-- Total compile time: {} s'.format(round(time() - self.start_time, 3)))
+        self.render_separator()
+        self.render_message('Success!')
+        self.render_message('Total compile time: {} s'.format(round(time() - self.start_time, 3)))
         return EXIT_SUCCESS
 
 
@@ -56,9 +65,8 @@ def main():
     pass
 
 @main.command(help='Compile binary')
-@option('-n', '--jobs', help='Allow specified number of make jobs at once', default=12)
-def compile(jobs):
-    sys.exit(CompileSocks(jobs).execute_main())
+def compile():
+    sys.exit(CompileSocks().execute_main())
 
 if __name__ == '__main__':
     main()
