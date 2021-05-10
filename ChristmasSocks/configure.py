6637f11898e6bd0f331ceaf55bcc1f93d4289613
David Weber <dsw7@sfu.ca>
2021-05-10 03:34:09 +0100

    Configure script

--- ChristmasSocks/configure.py
+++ ChristmasSocks/configure.py
@@ -4,43 +4,45 @@ import sys
 from os import get_terminal_size, path
 from subprocess import call
 from time import time
+from click import secho
 
 EXIT_SUCCESS = 0
 EXIT_FAILURE = 1
+TERMINAL_WIDTH = get_terminal_size().columns
+
+def render_separator() -> None:
+    secho('=' * TERMINAL_WIDTH, fg='cyan')
 
 
 class CompileSocks:
     def __init__(self) -> None:
-        self.hbar = '=' * get_terminal_size().columns
         self.path_this = path.dirname(__file__)
         self.start_time = time()
 
     def generate_makefiles(self) -> int:
+        render_separator()
         command = 'cmake -S {} -B {}/bin'.format(self.path_this, self.path_this)
         return call(command.split())
 
     def compile_binary_from_makefiles(self) -> int:
+        render_separator()
         command = 'make --jobs=12 -C {}/bin'.format(self.path_this)
         return call(command.split())
 
     def execute_main(self) -> None:
-        print(self.hbar)
-        print('-- Compiling project: ChristmasSocks')
-        print('-- Project maintainer: David Weber')
+        render_separator()
+        secho('-- Compiling project: ChristmasSocks')
+        secho('-- Project maintainer: David Weber')
 
-        print(self.hbar)
         if self.generate_makefiles() != EXIT_SUCCESS:
             return EXIT_FAILURE
 
-        print(self.hbar)
         if self.compile_binary_from_makefiles() != EXIT_SUCCESS:
             return EXIT_FAILURE
 
-        print(self.hbar)
-        print('-- Success!')
-        print('-- Total compile time: {} s'.format(round(time() - self.start_time, 3)))
-        print(self.hbar)
-
+        render_separator()
+        secho('-- Success!')
+        secho('-- Total compile time: {} s'.format(round(time() - self.start_time, 3)))
         return EXIT_SUCCESS
 
 
