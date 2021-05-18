8d460894d15ed21b0fbb37558f0e462709a3c519
David Weber <dsw7@sfu.ca>
2021-05-18 05:19:22 +0100

    Fix config_file

--- ChristmasSocks/src/config_file.cpp
+++ ChristmasSocks/src/config_file.cpp
@@ -1,11 +1,14 @@
 #include "config_file.h"
 
-ConfigParser::ConfigParser(std::string &path_config_file) {
-    this->path_config_file = path_config_file;
-    // this->global_configs should be set to something here... cppcheck is complaining (and rightly so)
+ConfigParser::ConfigParser(std::string &path_config_file): path_config_file(path_config_file)
+{
+    this->global_configs.tcp_port = TCP_PORT;
+    this->global_configs.max_num_connections_queue = MAX_NUM_CONNECTIONS_QUEUE;
+    this->global_configs.tcp_buffer_size = TCP_BUFFER_SIZE;
 }
 
-void ConfigParser::file_contents_to_raw_configs() {
+void ConfigParser::file_contents_to_raw_configs()
+{
     std::istringstream is_file(this->file_contents);
     std::string line;
 
@@ -23,25 +26,26 @@ void ConfigParser::file_contents_to_raw_configs() {
     }
 }
 
-void ConfigParser::raw_configs_to_global_configs() {
+void ConfigParser::raw_configs_to_global_configs()
+{
     /*
      * now map raw_configs to global_configs struct here
      */
 
-    this->global_configs.enable_debug_logging = ENABLE_DEBUG_LOGGING;  // this might need to go elsewhere
     this->global_configs.tcp_port = TCP_PORT;
     this->global_configs.max_num_connections_queue = MAX_NUM_CONNECTIONS_QUEUE;
     this->global_configs.tcp_buffer_size = TCP_BUFFER_SIZE;
 }
 
-void ConfigParser::extern_constants_to_global_configs() {
-    this->global_configs.enable_debug_logging = ENABLE_DEBUG_LOGGING;  // this might need to go elsewhere
+void ConfigParser::extern_constants_to_global_configs()
+{
     this->global_configs.tcp_port = TCP_PORT;
     this->global_configs.max_num_connections_queue = MAX_NUM_CONNECTIONS_QUEUE;
     this->global_configs.tcp_buffer_size = TCP_BUFFER_SIZE;
 }
 
-configs_t ConfigParser::load_configs() {
+configs_t ConfigParser::load_configs()
+{
     if (!file_exists(this->path_config_file)) {
         RootLogger::info("Could not find " + this->path_config_file + ". Using default configurations");
         this->extern_constants_to_global_configs();
