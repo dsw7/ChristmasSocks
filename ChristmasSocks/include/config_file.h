f07b0c2f194b9d0a48e8b8d630cc0cf9c35df4c7
David Weber <dsw7@sfu.ca>
2021-05-19 06:36:29 +0100

    Remove class

--- ChristmasSocks/include/config_file.h
+++ ChristmasSocks/include/config_file.h
@@ -18,20 +18,4 @@ void set_root_configs(configs_t &configs);
 void parse_config_file_contents(std::string &file_contents, std::map<std::string, std::string> &raw_configs);
 void overwrite_default_configs_with_config_file_configs(configs_t &configs, std::string &path_config_file);
 
-class ConfigParser
-{
-    private:
-        std::string path_config_file;
-        std::map<std::string, std::string> raw_configs;
-        std::string file_contents;
-        configs_t global_configs;
-
-    public:
-        ConfigParser(std::string &path_config_file);
-        void file_contents_to_raw_configs();
-        void overwrite_default_configs_with_config_file_configs();
-        void overwrite_configs_from_file_with_cli_options();
-        configs_t load_configs();
-};
-
 #endif
--- ChristmasSocks/src/main.cpp
+++ ChristmasSocks/src/main.cpp
@@ -84,8 +84,8 @@ int main()
     register_ipc_signals();
 
     configs_t global_configs;
-    //global_configs = ConfigParser(CONFIG_FILEPATH).load_configs();
     set_root_configs(global_configs);
+    overwrite_default_configs_with_config_file_configs(global_configs, CONFIG_FILEPATH);
 
     server_impl_main(global_configs);
     RootLogger::footer();
