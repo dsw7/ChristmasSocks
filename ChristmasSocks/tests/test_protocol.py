0da607055aea4579e66f323b87397c362dab7bd6
David Weber <dsw7@sfu.ca>
2021-07-12 23:24:35 -0700

    Rename test function

--- ChristmasSocks/tests/test_protocol.py
+++ ChristmasSocks/tests/test_protocol.py
@@ -67,7 +67,7 @@ class TestProtocolLimits(TestCase):
     def tearDown(self) -> None:
         self.client.disconnect()
 
-    def test_echo_almost_max_size_byte_string(self) -> None:
+    def test_echo_max_size__minus_one_byte_string(self) -> None:
         buffer_size = self.client.ini_configs['server'].getint('tcp_buffer_size') - 1
 
         for string in generate_random_string(num_strings=5, len_strings=buffer_size):
