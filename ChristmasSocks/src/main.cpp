e635943a77b7aac3b8d540d96fc7f3c4a7c632c2
David Weber <dsw7@sfu.ca>
2021-07-19 19:28:20 -0700

    Remove a header file

--- ChristmasSocks/src/main.cpp
+++ ChristmasSocks/src/main.cpp
@@ -1,4 +1,14 @@
-#include "main.h"
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/epoll.h>
+
+#include "logger.h"
+#include "server.h"
+#include "ipc_signal_registers.h"
+#include "clients.h"
+#include "sysconfig_pipeline.h"
+#include "header_footer.h"
 
 void server_impl_main(configs_t &configs)
 {
