216b77965ace840779d4bcc8ec15ab70bfdd9670
David Weber <dsw7@sfu.ca>
2021-07-30 00:09:01 -0700

    Add test

---
+++ ChristmasSocks/tests/test_release_command_line_interface.py
@@ -0,0 +1,28 @@
+# pylint: disable=C0103  # Disable snake case warning
+# pylint: disable=C0202  # Disable cls requirement for setUpClass / tearDownClass
+
+from unittest import TestCase
+from utils import (
+    Client,
+    Server,
+    generate_random_string,
+    generate_random_punctuation
+)
+
+
+class TestCommandLineInterface(TestCase):
+
+    @classmethod
+    def setUpClass(self) -> None:
+        self.client = Client()
+        self.client.connect()
+
+    @classmethod
+    def tearDownClass(self) -> None:
+        self.client.disconnect()
+        self.server.stop_server()
+
+    def test_invalid_port_low(self) -> None:
+        self.server = Server()
+        self.server.start_server('--port=22')
+        print(self.server.process.pid)
