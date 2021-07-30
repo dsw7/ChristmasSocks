f4ea52af73cacc7d11dd338a74f027dbf046c8e7
David Weber <dsw7@sfu.ca>
2021-07-30 00:33:01 -0700

    Add test

--- ChristmasSocks/tests/test_release_command_line_interface.py
+++ ChristmasSocks/tests/test_release_command_line_interface.py
@@ -27,3 +27,7 @@ class TestCommandLineInterface(TestCase):
     def test_invalid_port_high(self) -> None:
         self.server.start_server('--port=99999')
         self.assertIsNone(self.server.process.pid)
+
+    def test_valid_port(self) -> None:
+        self.server.start_server('--port=8080')
+        self.assertTrue(self.server.process.pid > 0)
