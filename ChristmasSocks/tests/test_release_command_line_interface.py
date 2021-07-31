9699a7da33f59756aba560c8adbbbb95c5f2ce11
David Weber <dsw7@sfu.ca>
2021-07-31 06:38:37 -0700

    cli tests

--- ChristmasSocks/tests/test_release_command_line_interface.py
+++ ChristmasSocks/tests/test_release_command_line_interface.py
@@ -1,32 +1,35 @@
-# pylint: disable=C0103  # Disable snake case warning
-# pylint: disable=C0202  # Disable cls requirement for setUpClass / tearDownClass
+# pylint: disable=W0201  # Disable defined outside __init__
 
-from unittest import TestCase
-from utils import Server, Client
+from pytest import (
+    mark,
+    raises
+)
+from utils import (
+    Server,
+    Client
+)
 
+@mark.release_test
+class TestCommandLineInterface:
 
-class TestCommandLineInterface(TestCase):
-
-    @classmethod
-    def setUpClass(self) -> None:
+    def setup_class(self) -> None:
         self.server = Server()
         self.client = Client()
 
-    @classmethod
-    def tearDown(self) -> None:
+    def teardown_method(self) -> None:
         self.server.stop_server()
         self.client.disconnect()
 
     def test_invalid_port_low_long_option(self) -> None:
         port = 22
         self.server.start_server('--port={}'.format(port))
-        with self.assertRaises(OSError):
+        with raises(OSError):
             self.client.connect(port=port)
 
     def test_invalid_port_high_long_option(self) -> None:
         port = 99999
         self.server.start_server('--port={}'.format(port))
-        with self.assertRaises(OSError):
+        with raises(OSError):
             self.client.connect(port=port)
 
     def test_valid_port_long_option(self) -> None:
@@ -34,18 +37,18 @@ class TestCommandLineInterface(TestCase):
         string = 'foobar'
         self.server.start_server('--port={}'.format(port))
         self.client.connect(port=port)
-        self.assertEqual(string, self.client.send(string))
+        assert string == self.client.send(string)
 
     def test_invalid_port_low_short_option(self) -> None:
         port = 22
         self.server.start_server('-p', str(port))
-        with self.assertRaises(OSError):
+        with raises(OSError):
             self.client.connect(port=port)
 
     def test_invalid_port_high_short_option(self) -> None:
         port = 99999
         self.server.start_server('-p', str(port))
-        with self.assertRaises(OSError):
+        with raises(OSError):
             self.client.connect(port=port)
 
     def test_valid_port_short_option(self) -> None:
@@ -53,4 +56,4 @@ class TestCommandLineInterface(TestCase):
         string = 'foobar'
         self.server.start_server('-p', str(port))
         self.client.connect(port=port)
-        self.assertEqual(string, self.client.send(string))
+        assert string == self.client.send(string)
