15163529a058d847a473c4835550c74c28f17023
David Weber <dsw7@sfu.ca>
2021-07-31 07:03:53 -0700

    protocol tests

--- ChristmasSocks/tests/test_release_protocol.py
+++ ChristmasSocks/tests/test_release_protocol.py
@@ -1,7 +1,6 @@
-# pylint: disable=C0103  # Disable snake case warning
-# pylint: disable=C0202  # Disable cls requirement for setUpClass / tearDownClass
+# pylint: disable=W0201  # Disable defined outside __init__
 
-from unittest import TestCase
+from pytest import mark
 from utils import (
     Client,
     Server,
@@ -9,42 +8,34 @@ from utils import (
     generate_random_punctuation
 )
 
+@mark.release_test
+class TestProtocolRandomStrings:
 
-class TestProtocolRandomStrings(TestCase):
-
-    @classmethod
-    def setUpClass(self) -> None:
-
-        # Tell unittest to print large strings (diffs)
-        self.maxDiff = None
-
+    def setup_class(self) -> None:
         self.server = Server()
         self.server.start_server()
         self.client = Client()
         self.client.connect()
         self.buffer_size = self.client.cfgs['client'].getint('tcp_buffer_size') - 1
 
-    @classmethod
-    def tearDownClass(self) -> None:
+    def teardown_class(self) -> None:
         self.client.disconnect()
         self.server.stop_server()
 
-    def test_echo_15_byte_string(self) -> None:
-        for string in generate_random_string(num_strings=5, len_strings=15):
-            with self.subTest():
-                self.assertEqual(string, self.client.send(string))
+    @mark.parametrize('string', generate_random_string(num_strings=5, len_strings=15))
+    def test_echo_15_byte_string(self, string) -> None:
+        assert string == self.client.send(string)
 
-    def test_echo_15_byte_punctuation(self) -> None:
-        for string in generate_random_punctuation(num_strings=5, len_strings=15):
-            with self.subTest():
-                self.assertEqual(string, self.client.send(string))
+    @mark.parametrize('string', generate_random_punctuation(num_strings=5, len_strings=15))
+    def test_echo_15_byte_punctuation(self, string) -> None:
+        assert string == self.client.send(string)
 
     def test_echo_max_size_minus_one_byte_string(self) -> None:
         string = generate_random_string(num_strings=1, len_strings=self.buffer_size)
-        self.assertEqual(string[0], self.client.send(string[0]))
+        assert string[0] == self.client.send(string[0])
 
     def test_echo_max_size_plus_five_bytes_string(self) -> None:
         string = generate_random_string(num_strings=1, len_strings=self.buffer_size + 5)
         first_chunk = string[0][0:1024]
         # XXX What happens to string[0][1024:] data?? looks like this could be a memory leak
-        self.assertEqual(first_chunk, self.client.send(string[0]))
+        assert first_chunk == self.client.send(string[0])
