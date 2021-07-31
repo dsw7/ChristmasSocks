74d978a4a317c9d1f3e4557df53d1a707fb4266e
David Weber <dsw7@sfu.ca>
2021-07-31 06:42:15 -0700

    Exit command

--- ChristmasSocks/tests/test_exit_command.py
+++ ChristmasSocks/tests/test_exit_command.py
@@ -1,30 +1,28 @@
-# pylint: disable=C0103  # Disable snake case warning
-# pylint: disable=C0202  # Disable cls requirement for setUpClass / tearDownClass
+# pylint: disable=W0201  # Disable defined outside __init__
 
-from unittest import TestCase
+from pytest import mark
 from utils import (
     Client,
     Server
 )
 
-class TestExitCommand(TestCase):
+@mark.release_test
+class TestExitCommand:
 
-    @classmethod
-    def setUpClass(self) -> None:
+    def setup_class(self) -> None:
         self.server = Server()
         self.server.start_server()
         self.client = Client()
         self.client.connect()
 
-    @classmethod
-    def tearDownClass(self) -> None:
+    def teardown_class(self) -> None:
         self.client.disconnect()
 
     def test_process_is_dead_after_sending_exit(self) -> None:
         string = 'foobar'
-        self.assertEqual(string, self.client.send(string))
+        assert string == self.client.send(string)
         self.client.send('exit')
 
         # Cannot simply check if /proc/self.server.process.pid no longer exists because
         # python keeps the process lingering as a zombie
-        self.assertEqual('', self.client.send(string))
+        assert self.client.send(string) == ''
