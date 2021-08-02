fcb80b7370da80dfb36201f4a4abc8c665b75d79
David Weber <dsw7@sfu.ca>
2021-08-01 21:07:11 -0700

    Remove redundant test

--- ChristmasSocks/tests/test_command_line_interface.py
+++ ChristmasSocks/tests/test_command_line_interface.py
@@ -20,24 +20,12 @@ class TestCommandLineInterface:
         self.server.stop_server()
         self.client.disconnect()
 
-    def test_invalid_port_low_long_option(self) -> None:
-        port = 22
-        self.server.start_server('--port={}'.format(port))
-        with raises(OSError):
-            self.client.connect(port=port)
-
     def test_valid_port_long_option(self) -> None:
         port = 8080
         string = 'foobar'
         self.server.start_server('--port={}'.format(port))
         self.client.connect(port=port)
         assert string == self.client.send(string)
-
-    def test_invalid_port_low_short_option(self) -> None:
-        port = 22
-        self.server.start_server('-p', str(port))
-        with raises(OSError):
-            self.client.connect(port=port)
 
     def test_valid_port_short_option(self) -> None:
         port = 8080
