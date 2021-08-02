6ab11db2cf9ff4d1d880797853de2c8e815a7a3c
David Weber <dsw7@sfu.ca>
2021-08-02 03:45:33 -0700

    Move MAX_EPOLL_EVENTS

--- ChristmasSocks/src/utilities/constants.h
+++ ChristmasSocks/src/utilities/constants.h
@@ -9,6 +9,7 @@
 extern unsigned int TCP_PORT;
 extern unsigned int MAX_NUM_CONNECTIONS_QUEUE;
 extern unsigned int TCP_BUFFER_SIZE;
+extern unsigned int MAX_EPOLL_EVENTS;
 
 // --------------------------------------------------------------
 // HARD CODED INTERNAL CONSTANTS
