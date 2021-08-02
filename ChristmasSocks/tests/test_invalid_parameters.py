4fbe8958c898bd2eb22d01b3b466347a28e95dbb
David Weber <dsw7@sfu.ca>
2021-08-01 23:31:01 -0700

    Add test

---
+++ ChristmasSocks/tests/test_invalid_parameters.py
@@ -0,0 +1,20 @@
+# pylint: disable=W0201  # Disable defined outside __init__
+
+from pytest import mark
+from utils import (
+    Server,
+    Client,
+    EXIT_FAILURE
+)
+
+@mark.release_test
+class TestHelpMenu:
+
+    def setup_class(self) -> None:
+        self.server = Server()
+
+    def test_invalid_tcp_port_low(self) -> None:
+        assert self.server.start_server_in_foreground('--port=22') == EXIT_FAILURE
+
+    def test_invalid_tcp_port_high(self) -> None:
+        assert self.server.start_server_in_foreground('--port=99999') == EXIT_FAILURE
