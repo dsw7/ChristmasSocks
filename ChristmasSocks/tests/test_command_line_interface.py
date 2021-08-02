7a32977123f6f27324bb642a817fc4edc999254a
David Weber <dsw7@sfu.ca>
2021-08-01 21:11:45 -0700

    Add buffer tests

--- ChristmasSocks/tests/test_command_line_interface.py
+++ ChristmasSocks/tests/test_command_line_interface.py
@@ -1,9 +1,6 @@
 # pylint: disable=W0201  # Disable defined outside __init__
 
-from pytest import (
-    mark,
-    raises
-)
+from pytest import mark
 from utils import (
     Server,
     Client
@@ -15,6 +12,7 @@ class TestCommandLineInterface:
     def setup_class(self) -> None:
         self.server = Server()
         self.client = Client()
+        self.test_string = 'foobar'
 
     def teardown_method(self) -> None:
         self.server.stop_server()
@@ -22,14 +20,24 @@ class TestCommandLineInterface:
 
     def test_valid_port_long_option(self) -> None:
         port = 8080
-        string = 'foobar'
         self.server.start_server('--port={}'.format(port))
         self.client.connect(port=port)
-        assert string == self.client.send(string)
+        assert self.test_string == self.client.send(self.test_string)
 
     def test_valid_port_short_option(self) -> None:
         port = 8080
-        string = 'foobar'
         self.server.start_server('-p', str(port))
         self.client.connect(port=port)
-        assert string == self.client.send(string)
+        assert self.test_string == self.client.send(self.test_string)
+
+    def test_buffer_size_long_option(self) -> None:
+        buffer_size = 1024
+        self.server.start_server('--buffer-size={}'.format(buffer_size))
+        self.client.connect()
+        assert self.test_string == self.client.send(self.test_string)
+
+    def test_buffer_size_short_option(self) -> None:
+        buffer_size = 1024
+        self.server.start_server('-b', str(buffer_size))
+        self.client.connect()
+        assert self.test_string == self.client.send(self.test_string)
