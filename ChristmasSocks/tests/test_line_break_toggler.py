7eb8c808999202400a9936fbcb621ae8cbd1eb0b
David Weber <dsw7@sfu.ca>
2021-08-07 22:26:53 -0700

    Remove test

--- ChristmasSocks/tests/test_line_break_toggler.py
+++ ChristmasSocks/tests/test_line_break_toggler.py
@@ -7,7 +7,7 @@ from utils import (
 )
 
 @mark.release_test
-class TestLineBreakToggler:
+class TestHandleLineBreaks:
 
     def setup_class(self) -> None:
         self.server = Server()
@@ -16,11 +16,6 @@ class TestLineBreakToggler:
     def teardown_method(self) -> None:
         self.client.disconnect()
         self.server.stop_server()
-
-    def test_no_handle_line_breaks(self) -> None:
-        self.server.start_server_in_background()
-        self.client.connect()
-        assert self.client.send('foobar') == 'foobar'
 
     def test_handle_line_feed(self) -> None:
         self.server.start_server_in_background()
@@ -32,12 +27,12 @@ class TestLineBreakToggler:
         self.client.connect()
         assert self.client.send('foobar\r') == 'foobar\r'
 
-    def test_handle_carriage_return_line_feed(self) -> None:
+    def test_handle_end_of_line(self) -> None:
         self.server.start_server_in_background()
         self.client.connect()
         assert self.client.send('foobar\r\n') == 'foobar\r\n'
 
-    def test_handle_no_end_of_line(self) -> None:
+    def test_handle_no_line_break(self) -> None:
         self.server.start_server_in_background()
         self.client.connect()
         assert self.client.send('foobar') == 'foobar'
