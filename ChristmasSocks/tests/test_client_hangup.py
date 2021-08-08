3ebf784a24cc01abd6e11e83a89e4cd853864e76
David Weber <dsw7@sfu.ca>
2021-08-07 21:46:42 -0700

    Add hangup test

---
+++ ChristmasSocks/tests/test_client_hangup.py
@@ -0,0 +1,38 @@
+# pylint: disable=W0201  # Disable defined outside __init__
+
+from concurrent import futures
+from pytest import mark
+from utils import (
+    Server,
+    Client,
+    EXIT_SUCCESS
+)
+
+@mark.release_test
+class TestClientHangup:
+
+    def setup_class(self) -> None:
+        self.server = Server()
+        self.server.start_server_in_background()
+        self.client = Client()
+        self.client.connect()
+
+    def start_server(self) -> int:
+        return self.server.start_server_in_foreground()
+
+    def hangup_client(self) -> int:
+        self.client.disconnect()
+        return EXIT_SUCCESS
+
+    def test_echo_3_connections(self) -> None:
+        workers, results = [], []
+        with futures.ThreadPoolExecutor() as executor:
+            workers.append(executor.submit(self.start_server))
+            workers.append(executor.submit(self.hangup_client))
+
+            if futures.wait(workers, return_when=futures.FIRST_COMPLETED):
+                for worker in workers:
+                    results.append(worker.result())
+
+        for result in results:
+            assert result == EXIT_SUCCESS
