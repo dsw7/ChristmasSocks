2cce59aef77bc5efffdf5873ba37aadd8aaaacd7
David Weber <dsw7@sfu.ca>
2021-08-08 06:48:53 -0700

    Update help menu

--- ChristmasSocks/src/server/commands.cpp
+++ ChristmasSocks/src/server/commands.cpp
@@ -5,12 +5,12 @@ namespace Commands {
     std::string command_help()
     {
         RootLogger::info("Received help command. Returning a list of internal commands to client");
-        return "List of commands:\n"
+        return "> List of commands:\n"
         "> help  = Get a list of commands\n"
         "> sleep = Sleep for a short delay\n"
         "> exit  = Shut down the server\n"
-        "\n"
-        "Unknown commands will simply be echoed back to the client";
+        "> \n"
+        "> Unknown commands will simply be echoed back to the client";
     }
 
     void command_sleep()
