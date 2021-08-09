afb5af7f7b596094be1c767d7d47f97b70429851
David Weber <dsw7@sfu.ca>
2021-08-08 23:18:09 -0700

    Add test

--- ChristmasSocks/tests/test_commands.py
+++ ChristmasSocks/tests/test_commands.py
@@ -26,3 +26,18 @@ class TestExitCommand:
         # Cannot simply check if /proc/self.server.process.pid no longer exists because
         # python keeps the process lingering as a zombie
         assert self.client.send(string) == ''
+
+@mark.release_test
+class TestCommands:
+
+    def setup_class(self) -> None:
+        self.server = Server()
+        self.server.start_server_in_background()
+        self.client = Client()
+        self.client.connect()
+
+    def teardown_class(self) -> None:
+        self.client.disconnect()
+
+    def test_sleep(self) -> None:
+        assert 'sleep' == self.client.send('sleep')
