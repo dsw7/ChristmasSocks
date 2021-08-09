640317b694dd27431989da9fd6886b3fac28a359
David Weber <dsw7@sfu.ca>
2021-08-08 23:22:39 -0700

    Change message

--- ChristmasSocks/src/server/commands.cpp
+++ ChristmasSocks/src/server/commands.cpp
@@ -17,7 +17,7 @@ namespace Commands {
     {
         RootLogger::info("Received sleep command. Sleeping for " + std::to_string(MAX_SLEEP_DURATION_USEC) + " microseconds");
         usleep(MAX_SLEEP_DURATION_USEC);
-        return "> Slept for " + std::to_string(MAX_SLEEP_DURATION_USEC) + " microseconds";
+        return "Success";
     }
 
 }
