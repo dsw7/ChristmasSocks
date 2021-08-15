87e62a7659d4861cc66228a75a69d023896d4ab1
David Weber <dsw7@sfu.ca>
2021-08-14 22:18:57 -0700

    Fix cli test

--- ChristmasSocks/tests/test_command_line_interface.py
+++ ChristmasSocks/tests/test_command_line_interface.py
@@ -2,7 +2,8 @@
 
 from pytest import mark
 from utils import (
-    Server,
+    ServerForeground,
+    ServerBackground,
     Client,
     EXIT_SUCCESS
 )
@@ -11,20 +12,20 @@ from utils import (
 class TestHelpMenu:
 
     def setup_class(self) -> None:
-        self.server = Server()
+        self.server = ServerForeground()
 
     def test_help_long_option(self) -> None:
-        assert self.server.start_server_in_foreground('--help') == EXIT_SUCCESS
+        assert self.server.start_server('--help') == EXIT_SUCCESS
 
     def test_help_short_option(self) -> None:
-        assert self.server.start_server_in_foreground('-h') == EXIT_SUCCESS
+        assert self.server.start_server('-h') == EXIT_SUCCESS
 
 
 @mark.release_test
 class TestCommandLineInterface:
 
     def setup_class(self) -> None:
-        self.server = Server()
+        self.server = ServerBackground()
         self.client = Client()
         self.test_string = 'foobar'
 
@@ -34,36 +35,36 @@ class TestCommandLineInterface:
 
     def test_valid_port_long_option(self) -> None:
         port = 8080
-        self.server.start_server_in_background('--port={}'.format(port))
+        self.server.start_server('--port={}'.format(port))
         self.client.connect(port=port)
         assert self.test_string == self.client.send(self.test_string)
 
     def test_valid_port_short_option(self) -> None:
         port = 8080
-        self.server.start_server_in_background('-p', str(port))
+        self.server.start_server('-p', str(port))
         self.client.connect(port=port)
         assert self.test_string == self.client.send(self.test_string)
 
     def test_buffer_size_long_option(self) -> None:
         buffer_size = 1024
-        self.server.start_server_in_background('--buffer-size={}'.format(buffer_size))
+        self.server.start_server('--buffer-size={}'.format(buffer_size))
         self.client.connect()
         assert self.test_string == self.client.send(self.test_string)
 
     def test_buffer_size_short_option(self) -> None:
         buffer_size = 1024
-        self.server.start_server_in_background('-b', str(buffer_size))
+        self.server.start_server('-b', str(buffer_size))
         self.client.connect()
         assert self.test_string == self.client.send(self.test_string)
 
     def test_bind_ip_long_option(self) -> None:
         bind_ip = '127.0.0.1'
-        self.server.start_server_in_background('--bind-ip={}'.format(bind_ip))
+        self.server.start_server('--bind-ip={}'.format(bind_ip))
         self.client.connect()
         assert self.test_string == self.client.send(self.test_string)
 
     def test_bind_ip_short_option(self) -> None:
         bind_ip = '127.0.0.1'
-        self.server.start_server_in_background('-i', str(bind_ip))
+        self.server.start_server('-i', str(bind_ip))
         self.client.connect()
         assert self.test_string == self.client.send(self.test_string)
