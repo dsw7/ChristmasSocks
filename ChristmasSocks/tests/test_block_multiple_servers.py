3c269ff43b97829446d0fa050082b026b74007a5
David Weber <dsw7@sfu.ca>
2021-08-15 04:10:17 -0700

    Small test

--- ChristmasSocks/tests/test_block_multiple_servers.py
+++ ChristmasSocks/tests/test_block_multiple_servers.py
@@ -1,5 +1,6 @@
 # pylint: disable=W0201  # Disable defined outside __init__
 
+from subprocess import DEVNULL
 from pytest import mark
 from utils import (
     ServerBackground,
@@ -25,5 +26,6 @@ class TestBlockSecondServer:
         secondary_server = ServerForeground()
         assert secondary_server.start_server(
             '--port={}'.format(self.port),
-            logfile='test_block_second_server_secondary.log'
+            #logfile='test_block_second_server_secondary.log'
+            logfile=DEVNULL
         ) == EXIT_FAILURE
