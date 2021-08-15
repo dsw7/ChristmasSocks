932a6edcadaad60acca1ef1f33a73c052d9fd632
David Weber <dsw7@sfu.ca>
2021-08-14 23:36:18 -0700

    Update invalid params test

--- ChristmasSocks/tests/test_invalid_parameters.py
+++ ChristmasSocks/tests/test_invalid_parameters.py
@@ -1,5 +1,6 @@
 # pylint: disable=W0201  # Disable defined outside __init__
 
+from inspect import stack
 from pytest import mark
 from utils import ServerForeground, EXIT_FAILURE
 
@@ -7,13 +8,16 @@ from utils import ServerForeground, EXIT_FAILURE
 class TestInvalidParameters:
 
     def setup_class(self) -> None:
-        self.server = ServerForeground(logfile='{}.log'.format(self.__name__))
+        self.server = ServerForeground()
 
     def test_invalid_tcp_port_low(self) -> None:
-        assert self.server.start_server('--port=22') == EXIT_FAILURE
+        logfile = '{}.log'.format(stack()[0][3])
+        assert self.server.start_server('--port=22', logfile=logfile) == EXIT_FAILURE
 
     def test_invalid_tcp_port_high(self) -> None:
-        assert self.server.start_server('--port=99999') == EXIT_FAILURE
+        logfile = '{}.log'.format(stack()[0][3])
+        assert self.server.start_server('--port=99999', logfile=logfile) == EXIT_FAILURE
 
     def test_invalid_tcp_buffer_size(self) -> None:
-        assert self.server.start_server('--buffer-size=4') == EXIT_FAILURE
+        logfile = '{}.log'.format(stack()[0][3])
+        assert self.server.start_server('--buffer-size=4', logfile=logfile) == EXIT_FAILURE
