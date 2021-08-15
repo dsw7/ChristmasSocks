12a4a77980373d4533d49bcf82a583082d7a537a
David Weber <dsw7@sfu.ca>
2021-08-14 23:55:10 -0700

    Fix ref

--- ChristmasSocks/tests/test_various_line_breaks.py
+++ ChristmasSocks/tests/test_various_line_breaks.py
@@ -1,8 +1,9 @@
 # pylint: disable=W0201  # Disable defined outside __init__
 
+from inspect import stack
 from pytest import mark
 from utils import (
-    Server,
+    ServerBackground,
     Client
 )
 
@@ -10,7 +11,7 @@ from utils import (
 class TestHandleLineBreaks:
 
     def setup_class(self) -> None:
-        self.server = Server()
+        self.server = ServerBackground()
         self.client = Client()
 
     def teardown_method(self) -> None:
@@ -18,21 +19,21 @@ class TestHandleLineBreaks:
         self.server.stop_server()
 
     def test_handle_line_feed(self) -> None:
-        self.server.start_server_in_background()
+        self.server.start_server(logfile='{}.log'.format(stack()[0][3]))
         self.client.connect()
         assert self.client.send('foobar\n') == 'foobar\n'
 
     def test_handle_carriage_return(self) -> None:
-        self.server.start_server_in_background()
+        self.server.start_server(logfile='{}.log'.format(stack()[0][3]))
         self.client.connect()
         assert self.client.send('foobar\r') == 'foobar\r'
 
     def test_handle_end_of_line(self) -> None:
-        self.server.start_server_in_background()
+        self.server.start_server(logfile='{}.log'.format(stack()[0][3]))
         self.client.connect()
         assert self.client.send('foobar\r\n') == 'foobar\r\n'
 
     def test_handle_no_line_break(self) -> None:
-        self.server.start_server_in_background()
+        self.server.start_server(logfile='{}.log'.format(stack()[0][3]))
         self.client.connect()
         assert self.client.send('foobar') == 'foobar'
