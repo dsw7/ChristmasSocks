ec3ce15aa215356bc13eb433079b78dcadeae0a0
David Weber <dsw7@sfu.ca>
2021-08-14 23:32:28 -0700

    Update invalid params test

--- ChristmasSocks/tests/test_invalid_parameters.py
+++ ChristmasSocks/tests/test_invalid_parameters.py
@@ -1,19 +1,19 @@
 # pylint: disable=W0201  # Disable defined outside __init__
 
 from pytest import mark
-from utils import Server, EXIT_FAILURE
+from utils import ServerForeground, EXIT_FAILURE
 
 @mark.release_test
 class TestInvalidParameters:
 
     def setup_class(self) -> None:
-        self.server = Server()
+        self.server = ServerForeground(logfile='{}.log'.format(self.__name__))
 
     def test_invalid_tcp_port_low(self) -> None:
-        assert self.server.start_server_in_foreground('--port=22') == EXIT_FAILURE
+        assert self.server.start_server('--port=22') == EXIT_FAILURE
 
     def test_invalid_tcp_port_high(self) -> None:
-        assert self.server.start_server_in_foreground('--port=99999') == EXIT_FAILURE
+        assert self.server.start_server('--port=99999') == EXIT_FAILURE
 
     def test_invalid_tcp_buffer_size(self) -> None:
-        assert self.server.start_server_in_foreground('--buffer-size=4') == EXIT_FAILURE
+        assert self.server.start_server('--buffer-size=4') == EXIT_FAILURE
