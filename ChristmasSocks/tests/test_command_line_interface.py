07c996c1408a5ae4495156db67575455e22f560d
David Weber <dsw7@sfu.ca>
2021-08-15 03:52:11 -0700

    Fix cli test

--- ChristmasSocks/tests/test_command_line_interface.py
+++ ChristmasSocks/tests/test_command_line_interface.py
@@ -28,8 +28,7 @@ class TestHelpMenu:
 class TestCommandLineInterface:
 
     def setup_class(self) -> None:
-        logfile = '{}.log'.format(self.__name__)
-        self.server = ServerBackground(logfile=logfile)
+        self.server = ServerBackground()
         self.client = Client()
         self.test_string = 'foobar'
 
@@ -38,37 +37,43 @@ class TestCommandLineInterface:
         self.client.disconnect()
 
     def test_valid_port_long_option(self) -> None:
+        logfile = '{}.log'.format(stack()[0][3])
         port = 8080
-        self.server.start_server('--port={}'.format(port))
+        self.server.start_server('--port={}'.format(port), logfile=logfile)
         self.client.connect(port=port)
         assert self.test_string == self.client.send(self.test_string)
 
     def test_valid_port_short_option(self) -> None:
+        logfile = '{}.log'.format(stack()[0][3])
         port = 8080
-        self.server.start_server('-p', str(port))
+        self.server.start_server('-p', str(port), logfile=logfile)
         self.client.connect(port=port)
         assert self.test_string == self.client.send(self.test_string)
 
     def test_buffer_size_long_option(self) -> None:
+        logfile = '{}.log'.format(stack()[0][3])
         buffer_size = 1024
-        self.server.start_server('--buffer-size={}'.format(buffer_size))
+        self.server.start_server('--buffer-size={}'.format(buffer_size), logfile=logfile)
         self.client.connect()
         assert self.test_string == self.client.send(self.test_string)
 
     def test_buffer_size_short_option(self) -> None:
+        logfile = '{}.log'.format(stack()[0][3])
         buffer_size = 1024
-        self.server.start_server('-b', str(buffer_size))
+        self.server.start_server('-b', str(buffer_size), logfile=logfile)
         self.client.connect()
         assert self.test_string == self.client.send(self.test_string)
 
     def test_bind_ip_long_option(self) -> None:
+        logfile = '{}.log'.format(stack()[0][3])
         bind_ip = '127.0.0.1'
-        self.server.start_server('--bind-ip={}'.format(bind_ip))
+        self.server.start_server('--bind-ip={}'.format(bind_ip), logfile=logfile)
         self.client.connect()
         assert self.test_string == self.client.send(self.test_string)
 
     def test_bind_ip_short_option(self) -> None:
+        logfile = '{}.log'.format(stack()[0][3])
         bind_ip = '127.0.0.1'
-        self.server.start_server('-i', str(bind_ip))
+        self.server.start_server('-i', str(bind_ip), logfile=logfile)
         self.client.connect()
         assert self.test_string == self.client.send(self.test_string)
