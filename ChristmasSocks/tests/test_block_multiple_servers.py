28f6e1cd52c2980d868b8a393864cea92083fdad
David Weber <dsw7@sfu.ca>
2021-08-14 23:15:02 -0700

    Fix block test

--- ChristmasSocks/tests/test_block_multiple_servers.py
+++ ChristmasSocks/tests/test_block_multiple_servers.py
@@ -1,19 +1,29 @@
 # pylint: disable=W0201  # Disable defined outside __init__
 
 from pytest import mark
-from utils import ServerBackground, ServerForeground, EXIT_FAILURE
+from utils import (
+    ServerBackground,
+    ServerForeground,
+    EXIT_FAILURE
+)
 
 @mark.release_test
 class TestBlockSecondServer:
 
     def setup_class(self) -> None:
         self.port = 8000
-        self.primary_server = ServerBackground(logfile='test_block_second_server_primary.log')
-        self.primary_server.start_server('--port={}'.format(self.port))
+        self.primary_server = ServerBackground()
+        self.primary_server.start_server(
+            '--port={}'.format(self.port),
+            logfile='test_block_second_server_primary.log'
+        )
 
     def teardown_class(self) -> None:
         self.primary_server.stop_server()
 
     def test_start_second_server(self) -> None:
-        secondary_server = ServerForeground(logfile='test_block_second_server_secondary.log')
-        assert secondary_server.start_server('--port={}'.format(self.port)) == EXIT_FAILURE
+        secondary_server = ServerForeground()
+        assert secondary_server.start_server(
+            '--port={}'.format(self.port),
+            logfile='test_block_second_server_secondary.log'
+        ) == EXIT_FAILURE
