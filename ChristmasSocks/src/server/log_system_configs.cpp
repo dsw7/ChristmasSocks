f1229e9d0260a0569e2bfdf3f05d684cec1f7ad4
David Weber <dsw7@sfu.ca>
2021-08-21 23:22:41 -0700

    Test

--- ChristmasSocks/src/server/log_system_configs.cpp
+++ ChristmasSocks/src/server/log_system_configs.cpp
@@ -2,4 +2,6 @@
 
 void log_system_configs(configs_t &sysconfigs)
 {
+    RootLogger::info("The following constants will be distributed to the server machinery:");
+    RootLogger::info("> tcp_port = " + std::to_string(sysconfigs.tcp_port));
 }
