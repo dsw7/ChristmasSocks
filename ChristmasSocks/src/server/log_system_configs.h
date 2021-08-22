32781249e1952aa578bcdf4adcaf9d213391cfe6
David Weber <dsw7@sfu.ca>
2021-08-21 23:14:14 -0700

    Add header file

---
+++ ChristmasSocks/src/server/log_system_configs.h
@@ -0,0 +1,10 @@
+#ifndef LOG_SYSTEM_CONFIGS_H
+#define LOG_SYSTEM_CONFIGS_H
+
+#include <string>
+#include "logger.h"
+#include "system_configurations.h"
+
+void log_system_configs(configs_t &sysconfigs);
+
+#endif
