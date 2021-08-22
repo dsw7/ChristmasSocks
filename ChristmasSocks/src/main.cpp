91c31d6352d4c005d390655982c8b3252336a67d
David Weber <dsw7@sfu.ca>
2021-08-21 23:17:01 -0700

    Update CMakeLists.txt

--- ChristmasSocks/src/main.cpp
+++ ChristmasSocks/src/main.cpp
@@ -3,6 +3,7 @@
 #include "help_menu.h"
 #include "logger.h"
 #include "ipc_signal_registers.h"
+#include "log_system_configs.h"
 #include "system_configurations.h"
 #include "header_footer.h"
 #include "epoll_server.h"
@@ -21,6 +22,8 @@ int main(int argc, char **argv)
     SystemConfigurations sysconfig_handle;
     sysconfig_handle.overwrite_root_configs_with_config_file_configs();
     configs_t sysconfigs = sysconfig_handle.overwrite_config_file_configs_with_cli_args(argc, argv);
+
+    log_system_configs(sysconfigs);
 
     ServerImplMain server(sysconfigs);
     server.incoming_client_setup();
