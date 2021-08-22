453a906a5cd22b4a673ef6f00876bf7f6b64678b
David Weber <dsw7@sfu.ca>
2021-08-21 23:28:13 -0700

    Add full panel

--- ChristmasSocks/src/server/log_system_configs.cpp
+++ ChristmasSocks/src/server/log_system_configs.cpp
@@ -3,5 +3,8 @@
 void log_system_configs(configs_t &sysconfigs)
 {
     RootLogger::info("The following constants will be distributed to the server machinery:");
-    RootLogger::info("> tcp_port = " + std::to_string(sysconfigs.tcp_port));
+    RootLogger::info("+ tcp_port =                  " + std::to_string(sysconfigs.tcp_port));
+    RootLogger::info("+ bind_ip  =                  " + sysconfigs.bind_ip);
+    RootLogger::info("+ tcp_buffer_size =           " + std::to_string(sysconfigs.tcp_buffer_size));
+    RootLogger::info("+ max_num_connections_queue = " + std::to_string(sysconfigs.max_num_connections_queue));
 }
