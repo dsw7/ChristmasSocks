e04687a03d2e901789cf72c16a744dc84cefc7fe
David Weber <dsw7@sfu.ca>
2021-10-24 22:37:36 -0700

    Prompt for user

--- ChristmasSocks/setup.sh
+++ ChristmasSocks/setup.sh
@@ -6,10 +6,14 @@ CONFIGURE_SCRIPT=$(dirname $0)/configure.py
 
 echo -e "\e[1m\e[4mChristmasSocks Remote Server Management Software\e[0m"
 
-if [ "$EUID" -ne 0 ]
+if [ "$EUID" -ne 0 ];
     then echo "Please run as root!"
     exit 1
 fi
 
+echo -e "Starting installation"
+echo -n "Please enter a valid Linux user: "
+read USER
+
 chmod +x ${CONFIGURE_SCRIPT}
-${CONFIGURE_SCRIPT} compile
+su - $USER -c ${CONFIGURE_SCRIPT} compile
