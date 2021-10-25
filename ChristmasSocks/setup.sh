c9c7a2ee18213853effc2d1d2f59d1e977ad3839
David Weber <dsw7@sfu.ca>
2021-10-24 22:50:05 -0700

    Add compile step

--- ChristmasSocks/setup.sh
+++ ChristmasSocks/setup.sh
@@ -3,6 +3,8 @@
 # ----------------------------------------
 
 CONFIGURE_SCRIPT=$(dirname $0)/configure.py
+FILEPATH_BINARY=$(dirname $0)/bin/socks
+DIR_BIN=/usr/bin/
 
 echo -e "\e[1m\e[4mChristmasSocks Remote Server Management Software\e[0m"
 
@@ -11,16 +13,21 @@ if [ $(id --user) -ne 0 ];
     exit 1
 fi
 
-echo -e "Starting installation"
+echo "Starting installation"
 echo -n "Please enter a valid Linux user: "
 read USER
 
 chmod +x ${CONFIGURE_SCRIPT}
 
-echo -e "Compiling binary..."
+echo "Compiling binary..."
 su $USER --command "${CONFIGURE_SCRIPT} compile"
 
 if [ $? -ne 0 ];
     then echo "Failed to compile binary!"
     exit 1
 fi
+
+echo "Placing binary into system bin directory"
+echo "${FILEPATH_BINARY} -> ${DIR_BIN}"
+cp ${FILEPATH_BINARY} ${DIR_BIN}
+chown $USER ${FILEPATH_BINARY}
