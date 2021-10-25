d648ae3df393d4a148f4a387d42a9b7e434e6df6
David Weber <dsw7@sfu.ca>
2021-10-24 22:42:34 -0700

    Fix script

--- ChristmasSocks/setup.sh
+++ ChristmasSocks/setup.sh
@@ -6,7 +6,7 @@ CONFIGURE_SCRIPT=$(dirname $0)/configure.py
 
 echo -e "\e[1m\e[4mChristmasSocks Remote Server Management Software\e[0m"
 
-if [ $(id -u) -ne 0 ];
+if [ $(id --user) -ne 0 ];
     then echo "Please run as root!"
     exit 1
 fi
@@ -16,4 +16,4 @@ echo -n "Please enter a valid Linux user: "
 read USER
 
 chmod +x ${CONFIGURE_SCRIPT}
-su - $USER -c ${CONFIGURE_SCRIPT} compile
+su $USER --command "${CONFIGURE_SCRIPT} compile"
