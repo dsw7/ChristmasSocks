fe7b672c1d0847645b6964e9656ea7fdc2251914
David Weber <dsw7@sfu.ca>
2021-10-24 22:53:30 -0700

    Add copy step

--- ChristmasSocks/setup.sh
+++ ChristmasSocks/setup.sh
@@ -7,13 +7,14 @@ FILEPATH_BINARY=$(dirname $0)/bin/socks
 DIR_BIN=/usr/bin/
 
 echo -e "\e[1m\e[4mChristmasSocks Remote Server Management Software\e[0m"
+echo
 
 if [ $(id --user) -ne 0 ];
     then echo "Please run as root!"
     exit 1
 fi
 
-echo "Starting installation"
+echo "Starting installation..."
 echo -n "Please enter a valid Linux user: "
 read USER
 echo
@@ -22,19 +23,19 @@ chmod +x ${CONFIGURE_SCRIPT}
 
 echo "Compiling binary..."
 su $USER --command "${CONFIGURE_SCRIPT} compile"
-
 if [ $? -ne 0 ];
     then echo "Failed to compile binary!"
     exit 1
 fi
+echo
 
-echo "Copying binary into system bin directory"
+echo "Copying binary into system bin directory..."
 echo "${FILEPATH_BINARY} -> ${DIR_BIN}"
 cp ${FILEPATH_BINARY} ${DIR_BIN}
 if [ $? -ne 0 ];
     then echo "Failed to copy binary!"
     exit 1
 fi
-
 echo
+
 chown $USER ${FILEPATH_BINARY}
