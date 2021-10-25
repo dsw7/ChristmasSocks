672e61ec2c4869133a20f0eac1a19a59179f14eb
David Weber <dsw7@sfu.ca>
2021-10-24 22:21:34 -0700

    Add compile step to setup script

--- ChristmasSocks/setup.sh
+++ ChristmasSocks/setup.sh
@@ -2,9 +2,14 @@
 # ChristmasSocks Remote Server Management Software
 # ----------------------------------------
 
+CONFIGURE_SCRIPT=$(dirname $0)/configure.py
+
 echo -e "\e[1m\e[4mChristmasSocks Remote Server Management Software\e[0m"
 
 if [ "$EUID" -ne 0 ]
     then echo "Please run as root!"
     exit 1
 fi
+
+chmod +x ${CONFIGURE_SCRIPT}
+${CONFIGURE_SCRIPT} compile
