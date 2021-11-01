b0becd77bcbc27294e8e31ac46e6e399c0e25e9c
David Weber <dsw7@sfu.ca>
2021-11-01 00:17:52 -0700

    Fix compile issue

--- ChristmasSocks/src/server/frontend/parse_config_file.h
+++ ChristmasSocks/src/server/frontend/parse_config_file.h
@@ -4,6 +4,6 @@
 #include <string>
 #include "command_line_interface.h" // access Configs struct
 
-bool read_configs_from_file(std::string path_config_file, Configs &configs)
+bool read_configs_from_file(std::string path_config_file, Configs &configs);
 
 #endif
