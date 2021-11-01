72297e857f5ccecab55310aa33eb4d1ce2277408
David Weber <dsw7@sfu.ca>
2021-10-31 23:29:52 -0700

    Add config file parse base

---
+++ ChristmasSocks/src/server/frontend/parse_config_file.cpp
@@ -0,0 +1,6 @@
+#include "parse_config_file.h"
+
+bool read_configs_from_file(int argc, char **argv)
+{
+    return true;
+}
---
+++ ChristmasSocks/src/server/frontend/parse_config_file.h
@@ -0,0 +1,5 @@
+#ifndef PARSE_CONFIG_FILE_H
+#define PARSE_CONFIG_FILE_H
+bool read_configs_from_file(int argc, char **argv);
+
+#endif
