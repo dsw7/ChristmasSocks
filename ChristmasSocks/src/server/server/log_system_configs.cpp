4fda13085e59fcd2cf8e18e911a6e77cf641122f
David Weber <dsw7@sfu.ca>
2021-11-02 19:45:52 -0700

    Change log system configs format

--- ChristmasSocks/src/server/server/log_system_configs.cpp
+++ ChristmasSocks/src/server/server/log_system_configs.cpp
@@ -3,9 +3,9 @@
 void log_system_configs(Configs &configs)
 {
     RootLogger::info("The following constants will be distributed to the server machinery:");
-    RootLogger::info("+ tcp_port =        " + std::to_string(configs.tcp_port));
-    RootLogger::info("+ bind_ip  =        " + configs.bind_ip);
-    RootLogger::info("+ tcp_buffer_size = " + std::to_string(configs.tcp_buffer_size));
-    RootLogger::info("+ backlog =         " + std::to_string(configs.backlog));
-    RootLogger::info("+ master =          " + configs.master);
+    RootLogger::info("> tcp_port = " + std::to_string(configs.tcp_port));
+    RootLogger::info("> bind_ip  = " + configs.bind_ip);
+    RootLogger::info("> tcp_buffer_size = " + std::to_string(configs.tcp_buffer_size));
+    RootLogger::info("> backlog = " + std::to_string(configs.backlog));
+    RootLogger::info("> master = " + configs.master);
 }
