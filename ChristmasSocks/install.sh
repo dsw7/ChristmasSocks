a3048bd86a562f3ce407b0c5a71613909cce45ff
David Weber <dsw7@sfu.ca>
2021-11-03 02:12:05 -0700

    Add sed stage

--- ChristmasSocks/install.sh
+++ ChristmasSocks/install.sh
@@ -4,13 +4,14 @@
 
 CONFIGURE_SCRIPT=$(dirname $0)/configure.py
 FILEPATH_BINARY=$(dirname $0)/bin/socks
-DIR_BIN=/usr/bin/
+DIR_BIN=/usr/bin
 SERVICE_FILE=socks.service
 CONFIG_FILE=socks.ini
 SERVICE_NAME=socks.service
 PATH_SERVICE_FILE=$(dirname $0)/systemd/${SERVICE_FILE}
 PATH_CONFIG_FILE=$(dirname $0)/src/server/configs/${CONFIG_FILE}
-PATH_SYSTEMCTL=/etc/systemd/system/
+PATH_SYSTEMCTL=/etc/systemd/system
+DIR_CONFIG=/etc
 
 echo -e "\e[1m\e[4mChristmasSocks Remote Server Management Software\e[0m"
 
@@ -37,7 +38,7 @@ if [ $? -ne 0 ];
 fi
 
 echo "Copying binary into system bin directory..."
-cp -v ${FILEPATH_BINARY} ${DIR_BIN}
+cp -v ${FILEPATH_BINARY} ${DIR_BIN}/
 if [ $? -ne 0 ];
     then echo "Failed to copy binary!"
     exit 1
@@ -49,22 +50,29 @@ if [ $? -ne 0 ];
     exit 1
 fi
 
-echo "Copying ${CONFIG_FILE} to /etc/"
-cp -v ${PATH_CONFIG_FILE} /etc/
+echo "Copying ${CONFIG_FILE} to ${DIR_CONFIG}/"
+cp -v ${PATH_CONFIG_FILE} ${DIR_CONFIG}/
 if [ $? -ne 0 ];
     then echo "Failed to copy configuration file!"
     exit 1
 fi
 
 echo "Preparing ChristmasSocks service..."
-cp -v ${PATH_SERVICE_FILE} ${PATH_SYSTEMCTL}
+cp -v ${PATH_SERVICE_FILE} ${PATH_SYSTEMCTL}/
 if [ $? -ne 0 ];
     then echo "Failed to copy service file!"
     exit 1
 fi
 
-echo "Updating ${SERVICE_FILE} to match specified user..."
+echo "Updating ${SERVICE_FILE} User field..."
 sed -i "s/username/${USER}/" ${PATH_SYSTEMCTL}/${SERVICE_FILE}
+if [ $? -ne 0 ];
+    then echo "Failed to update ${SERVICE_FILE}!"
+    exit 1
+fi
+
+echo "Updating ${SERVICE_FILE} ExecStart command"
+sed -i "s/execstart/${DIR_BIN}\/socks --config ${DIR_CONFIG}\/${CONFIG_FILE}/" ${PATH_SYSTEMCTL}/${SERVICE_FILE}
 if [ $? -ne 0 ];
     then echo "Failed to update ${SERVICE_FILE}!"
     exit 1
