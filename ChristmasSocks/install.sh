3484f547edfd7a0e95e4d273d283b34d5bebaf98
David Weber <dsw7@sfu.ca>
2021-11-03 23:37:37 -0700

    Remove excess verbosity

--- ChristmasSocks/install.sh
+++ ChristmasSocks/install.sh
@@ -12,7 +12,7 @@ PATH_CONFIG_FILE=$(dirname $0)/src/server/configs/${CONFIG_FILE}
 PATH_SYSTEMCTL=/etc/systemd/system
 DIR_CONFIG=/etc
 
-echo -e "\e[1m\e[4mChristmasSocks Remote Server Management Software\e[0m"
+echo -e "\e[1m\e[4mChristmasSocks Remote Server Management Software\e[0m\n"
 
 if [ $(id --user) -ne 0 ];
     then echo "Please run as root!"
@@ -26,43 +26,37 @@ read USER
 echo "Compiling binary..."
 su ${USER} -c "cmake -S $(dirname $0) -B $(dirname $0)/bin && make --jobs=12 -C $(dirname $0)/bin"
 if [ $? -ne 0 ];
-    then echo "Failed to compile binary!"
-    exit 1
+    then exit 1
 fi
 
 echo "Copying binary into system bin directory..."
 cp -v ${FILEPATH_BINARY} ${DIR_BIN}/
 if [ $? -ne 0 ];
-    then echo "Failed to copy binary!"
-    exit 1
+    then exit 1
 fi
 
 echo "Copying ${CONFIG_FILE} to ${DIR_CONFIG}/"
 cp -v ${PATH_CONFIG_FILE} ${DIR_CONFIG}/
 if [ $? -ne 0 ];
-    then echo "Failed to copy configuration file!"
-    exit 1
+    then exit 1
 fi
 
 echo "Preparing ChristmasSocks service..."
 cp -v ${PATH_SERVICE_FILE} ${PATH_SYSTEMCTL}/
 if [ $? -ne 0 ];
-    then echo "Failed to copy service file!"
-    exit 1
+    then exit 1
 fi
 
 echo "Updating ${SERVICE_FILE} User field..."
 sed -i "s/username/${USER}/" ${PATH_SYSTEMCTL}/${SERVICE_FILE}
 if [ $? -ne 0 ];
-    then echo "Failed to update ${SERVICE_FILE}!"
-    exit 1
+    then exit 1
 fi
 
 echo "Updating ${SERVICE_FILE} ExecStart command"
 sed -i "s@execstart@${DIR_BIN}\/socks --config ${DIR_CONFIG}\/${CONFIG_FILE}@" ${PATH_SYSTEMCTL}/${SERVICE_FILE}
 if [ $? -ne 0 ];
-    then echo "Failed to update ${SERVICE_FILE}!"
-    exit 1
+    then exit 1
 fi
 
 systemctl daemon-reload
