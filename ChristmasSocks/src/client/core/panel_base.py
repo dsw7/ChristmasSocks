9b419456d676225d214709605787060424d66eae
David Weber <dsw7@sfu.ca>
2021-11-25 06:59:59 +0000

    Separate into subwins

--- ChristmasSocks/src/client/core/panel_base.py
+++ ChristmasSocks/src/client/core/panel_base.py
@@ -9,20 +9,23 @@ class ControlPanelBase(ABC):
         self.stdscr = stdscr
         self.rows, self.columns = self.stdscr.getmaxyx()
 
-    def header(self) -> None:
-        self.stdscr.addstr(0, 0, u'\u2500' * self.columns)
-        self.stdscr.addstr(1, (self.columns // 2) - (len(PROJECT_TITLE) // 2), PROJECT_TITLE, curses.A_BOLD)
-        self.stdscr.addstr(2, 0, u'\u2500' * self.columns)
+        # The args follow: lines, columns, y, x)
+        self.header = self.stdscr.subwin(3, self.columns, 0, 0)
+        self.footer = self.stdscr.subwin(3, self.columns, self.rows - 3, 0)
 
-    def footer(self) -> None:
-        self.stdscr.addstr(self.rows - 1, 1, ' Press any button to exit ', curses.A_REVERSE)
+    def display_header(self) -> None:
+        self.header.addstr(1, (self.columns // 2) - (len(PROJECT_TITLE) // 2), PROJECT_TITLE, curses.A_BOLD)
+        self.header.box()
+
+    def display_footer(self) -> None:
+        self.footer.addstr(1, 2, ' Press any button to exit ', curses.A_REVERSE)
+        self.footer.box()
 
     @abstractmethod
     def core(self) -> None:
         pass
 
     def main(self) -> None:
-        self.header()
-        self.footer()
-        self.stdscr.refresh()
+        self.display_header()
+        self.display_footer()
         self.core()
