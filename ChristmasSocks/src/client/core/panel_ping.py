9d23af063bd1586d7a7eb7efc99a30867768c7ed
David Weber <dsw7@sfu.ca>
2021-11-24 23:33:48 -0800

    Add placeholder header

--- ChristmasSocks/src/client/core/panel_ping.py
+++ ChristmasSocks/src/client/core/panel_ping.py
@@ -3,7 +3,11 @@ from core.panel_base import ControlPanelBase
 
 class PanelPing(ControlPanelBase):
 
+    def render_subwin_header(self) -> None:
+        self.body.addstr(1, 2, 'Example header')
+
     def core(self) -> None:
+        self.render_subwin_header()
         self.stdscr.getch()
 
 # See https://docs.python.org/3/howto/curses.html#starting-and-ending-a-curses-application
