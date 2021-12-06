a6ef95e5701d48b7143f649905469e5de3632d7c
David Weber <dsw7@sfu.ca>
2021-12-06 09:40:53 +0000

    Add new command to help messages

--- ChristmasSocks/src/server/commands/command_help.cpp
+++ ChristmasSocks/src/server/commands/command_help.cpp
@@ -6,10 +6,11 @@ namespace Commands {
     {
         RootLogger::info("Received help command. Returning a list of internal commands to client");
         return "> List of commands:\n"
-        "> help   = Get a list of commands\n"
-        "> sleep  = Sleep for a short delay\n"
-        "> uptime = Return time since boot in HH:MM:SS format\n"
-        "> exit   = Shut down the server\n"
+        "> help    = Get a list of commands\n"
+        "> sleep   = Sleep for a short delay\n"
+        "> uptime  = Return time since boot in HH:MM:SS format\n"
+        "> sysinfo = Return information about the host operating system\n"
+        "> exit    = Shut down the server\n"
         "> \n"
         "> Unknown commands will simply be echoed back to the client";
     }
