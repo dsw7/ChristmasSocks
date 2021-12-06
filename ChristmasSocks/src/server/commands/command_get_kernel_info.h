a8c31c651f2835a7185d7ff6ae98cea2a206944d
David Weber <dsw7@sfu.ca>
2021-12-06 09:20:24 +0000

    Add includes

--- ChristmasSocks/src/server/commands/command_get_kernel_info.h
+++ ChristmasSocks/src/server/commands/command_get_kernel_info.h
@@ -1,6 +1,10 @@
 #ifndef COMMAND_GET_KERNEL_INFO_H
 #define COMMAND_GET_KERNEL_INFO_H
 
-
+#include <string>
+#include <vector>
+#include <sys/utsname.h> // For utsname struct and uname function
+#include <sstream>       // For ostringstream
+#include <iterator>      // For ostream_iterator
 
 #endif
