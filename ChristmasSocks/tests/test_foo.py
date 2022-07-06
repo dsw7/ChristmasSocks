96e74cc18688a52bafbeeb91b829fe1125921169
David Weber <dsw7@sfu.ca>
2022-07-06 00:58:31 -0700

    Replace test_commands tests

--- ChristmasSocks/tests/test_foo.py
+++ ChristmasSocks/tests/test_foo.py
@@ -1,3 +1,28 @@
+import platform
+from re import compile
 
-def test_1(socks_server_background, socks_client):
-    assert True
+def test_sleep(socks_server_background, socks_client) -> None:
+    assert socks_client.send('sleep') == 'Success'
+
+def test_help(socks_server_background, socks_client) -> None:
+    assert '> List of commands:' in socks_client.send('help')
+
+def test_uptime(socks_server_background, socks_client) -> None: # Ensure that a string in HH:MM:SS form is returned
+    pattern = compile(r'[0-9]{2}:[0-9]{2}:[0-9]{2}')
+    assert pattern.match(socks_client.send('uptime'))
+
+def test_get_kernel_info(socks_server_background, socks_client) -> None:
+    entries = {}
+
+    for entry in socks_client.send('sysinfo').split('\n'):
+        if not entry:
+            continue
+
+        key, value = entry.split(': ')
+        entries[key] = value
+
+    assert entries['System'] == platform.system()
+    assert entries['Node'] == platform.node()
+    assert entries['Release'] == platform.release()
+    assert entries['Version'] == platform.version()
+    assert entries['Machine'] == platform.machine()
