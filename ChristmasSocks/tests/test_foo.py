1089f9bf84c1bbee22b034e46bb523b17dfcaec4
David Weber <dsw7@sfu.ca>
2022-07-06 01:01:44 -0700

    Add marker

--- ChristmasSocks/tests/test_foo.py
+++ ChristmasSocks/tests/test_foo.py
@@ -1,16 +1,21 @@
 import platform
 from re import compile
+from pytest import mark
 
+@mark.release_test
 def test_sleep(socks_server_background, socks_client) -> None:
     assert socks_client.send('sleep') == 'Success'
 
+@mark.release_test
 def test_help(socks_server_background, socks_client) -> None:
     assert '> List of commands:' in socks_client.send('help')
 
+@mark.release_test
 def test_uptime(socks_server_background, socks_client) -> None: # Ensure that a string in HH:MM:SS form is returned
     pattern = compile(r'[0-9]{2}:[0-9]{2}:[0-9]{2}')
     assert pattern.match(socks_client.send('uptime'))
 
+@mark.release_test
 def test_get_kernel_info(socks_server_background, socks_client) -> None:
     entries = {}
 
