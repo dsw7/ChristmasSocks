fca541742ca0b13d2f21d65d40c7a434b02457a0
David Weber <dsw7@sfu.ca>
2022-07-06 01:03:43 -0700

    Rename test and file

--- ChristmasSocks/tests/test_non_exit_commands.py
+++ ChristmasSocks/tests/test_non_exit_commands.py
@@ -3,20 +3,20 @@ from re import compile
 from pytest import mark
 
 @mark.release_test
-def test_sleep(socks_server_background, socks_client) -> None:
+def test_command_sleep(socks_server_background, socks_client) -> None:
     assert socks_client.send('sleep') == 'Success'
 
 @mark.release_test
-def test_help(socks_server_background, socks_client) -> None:
+def test_command_help(socks_server_background, socks_client) -> None:
     assert '> List of commands:' in socks_client.send('help')
 
 @mark.release_test
-def test_uptime(socks_server_background, socks_client) -> None: # Ensure that a string in HH:MM:SS form is returned
+def test_command_uptime(socks_server_background, socks_client) -> None: # Ensure that a string in HH:MM:SS form is returned
     pattern = compile(r'[0-9]{2}:[0-9]{2}:[0-9]{2}')
     assert pattern.match(socks_client.send('uptime'))
 
 @mark.release_test
-def test_get_kernel_info(socks_server_background, socks_client) -> None:
+def test_command_get_kernel_info(socks_server_background, socks_client) -> None:
     entries = {}
 
     for entry in socks_client.send('sysinfo').split('\n'):
