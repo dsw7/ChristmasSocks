990df8d8776987e08f571dd77e13e9a5bf27a048
David Weber <dsw7@sfu.ca>
2022-07-06 22:41:31 -0700

    Update release protocol test

---
+++ ChristmasSocks/tests/test_release_protocol_2.py
@@ -0,0 +1,26 @@
+from pytest import mark
+import utils
+
+self.buffer_size = socks_client.configs['client'].getint('tcp_buffer_size') - 1
+
+@mark.release_test
+@mark.parametrize('string', utils.generate_random_string(num_strings=10, len_strings=15))
+def test_echo_15_byte_string(socks_server_background, socks_client, string) -> None:
+    assert string == socks_client.send(string)
+
+@mark.release_test
+@mark.parametrize('string', utils.generate_random_punctuation(num_strings=10, len_strings=15))
+def test_echo_15_byte_punctuation(socks_server_background, socks_client, string) -> None:
+    assert string == socks_client.send(string)
+
+@mark.release_test
+def test_echo_max_size_minus_one_byte_string(socks_server_background, socks_client) -> None:
+    string = utils.generate_random_string(num_strings=1, len_strings=self.buffer_size)
+    assert string[0] == socks_client.send(string[0])
+
+@mark.release_test
+def test_echo_max_size_plus_five_bytes_string(socks_server_background, socks_client) -> None:
+    string = utils.generate_random_string(num_strings=1, len_strings=self.buffer_size + 5)
+    first_chunk = string[0][0:1024]
+    # XXX What happens to string[0][1024:] data?? looks like this could be a memory leak
+    assert first_chunk == socks_client.send(string[0])
