c166d49567f8b6513a203db4e89a56689dfa2445
David Weber <dsw7@sfu.ca>
2022-07-06 22:24:47 -0700

    Update line break test

--- ChristmasSocks/tests/test_various_line_breaks.py
+++ ChristmasSocks/tests/test_various_line_breaks.py
@@ -1,39 +1,17 @@
-# pylint: disable=W0201  # Disable defined outside __init__
-
-from inspect import stack
 from pytest import mark
-from utils import (
-    ServerBackground,
-    Client
-)
 
 @mark.release_test
-class TestHandleLineBreaks:
+def test_handle_line_feed(socks_server_background, socks_client) -> None:
+    assert socks_client.send('foobar\n') == 'foobar\n'
 
-    def setup_class(self) -> None:
-        self.server = ServerBackground()
-        self.client = Client()
+@mark.release_test
+def test_handle_carriage_return(socks_server_background, socks_client) -> None:
+    assert socks_client.send('foobar\r') == 'foobar\r'
 
-    def teardown_method(self) -> None:
-        self.client.disconnect()
-        self.server.stop_server()
+@mark.release_test
+def test_handle_end_of_line(socks_server_background, socks_client) -> None:
+    assert socks_client.send('foobar\r\n') == 'foobar\r\n'
 
-    def test_handle_line_feed(self) -> None:
-        self.server.start_server(logfile='{}.log'.format(stack()[0][3]))
-        self.client.connect()
-        assert self.client.send('foobar\n') == 'foobar\n'
-
-    def test_handle_carriage_return(self) -> None:
-        self.server.start_server(logfile='{}.log'.format(stack()[0][3]))
-        self.client.connect()
-        assert self.client.send('foobar\r') == 'foobar\r'
-
-    def test_handle_end_of_line(self) -> None:
-        self.server.start_server(logfile='{}.log'.format(stack()[0][3]))
-        self.client.connect()
-        assert self.client.send('foobar\r\n') == 'foobar\r\n'
-
-    def test_handle_no_line_break(self) -> None:
-        self.server.start_server(logfile='{}.log'.format(stack()[0][3]))
-        self.client.connect()
-        assert self.client.send('foobar') == 'foobar'
+@mark.release_test
+def test_handle_no_line_break(socks_server_background, socks_client) -> None:
+    assert socks_client.send('foobar') == 'foobar'
